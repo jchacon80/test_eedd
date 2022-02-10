12cb595434aa08a2f744eef65102a76086df0c46
jorge <jchacon80@gmail.com>
2022-02-10 19:02:28 +0100

    commit desde vs code

--- src/test/main/HelloWorld.java
+++ src/test/main/HelloWorld.java
@@ -3,7 +3,7 @@ package test.main;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		//esto es una prueba
+		//esto es una prueba java con GIT
 		System.out.println("hola mundo");
 	}
 }
